82aa084a5a8dc8702a563db2443ab3215e9dbd75
Piotr Radziszewski <radnet.makis@o2.pl>
2017-05-02 12:31:38 +0200

    nowy plik

---
+++ sidebar_left.php
@@ -0,0 +1,8 @@
+            
+			<div id="sidebar-left" class="sidenav">
+			    <p><a href="#">Link</a></p>
+			    <p><a href="#">Link</a></p>
+			    <p><a href="#">Link</a></p>
+			</div>
+			<div id="main">
+				
